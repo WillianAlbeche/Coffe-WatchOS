2799de1f87b423dd15301eedf2b75dc68287663e
Alex <Alex>
2022-04-08 15:15:36 +0100

    Next button sends to the next step

--- Coffee&Watch/Coffee&Watch WatchKit Extension/View/RecipeSteps/StepSuperView.swift
+++ Coffee&Watch/Coffee&Watch WatchKit Extension/View/RecipeSteps/StepSuperView.swift
@@ -8,16 +8,26 @@
 import SwiftUI
 
 struct StepSuperView: View {
-    @State var stepProgress = CGFloat(0)
-    @State var recipeProgress = 0.0
-
+//    var recipeId: Int
     var steps = LocalData.stepsDTO
+
+    @State var recipeProgress = 0.0
+    @State var stepProgress = CGFloat(0)
+
+    private let timer = Timer.publish(every: 4, on: .main, in: .common).autoconnect()
+    @State private var currentIndex = 0
+
+    private func goToNextStep() {
+        withAnimation {
+            currentIndex = currentIndex < steps.count ? currentIndex + 1 : 0
+        }
+    }
 
     var body: some View {
         GeometryReader { proxy in
             VStack(spacing: 12) {
                 ProgressBar(totalStepsRecipe: steps.count, percentProgress: $recipeProgress)
-                TabView {
+                TabView(selection: $currentIndex) {
                     ForEach(Array(zip(steps.indices, steps)), id: \.0) { index, step in
                         StepInstructionView(step: step)
                             .tag(index)
@@ -26,9 +36,16 @@ struct StepSuperView: View {
                 .tabViewStyle(.page(indexDisplayMode: .never))
                 .tabViewStyle(PageTabViewStyle())
                 .frame(width: proxy.size.width, height: proxy.size.height * 0.73)
-                ProgressButton(stepProgress: $stepProgress)
-                    .frame(width: proxy.size.width * 0.93)
+                Button(action: {
+                    goToNextStep()
+                }, label: {
+                    ProgressButton(stepProgress: $stepProgress)
+                        .frame(width: proxy.size.width * 0.93)
+                })
             }
+            .onReceive(timer, perform: { _ in
+                goToNextStep()
+            })
         }
     }
 }
